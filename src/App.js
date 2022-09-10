72566f82632d5d39b3c68c25b4a20db50c0730b5
Adham ElGazoly <elgazoly11@gmail.com>
2022-09-10 15:40:48 +0200

    Add Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@
 // import Landing from './components/landing/Landing';
 //import Login from './components/login/Login';
 import JobSearch from './components/job-search/JobSearch';
+import Offers from './components/offers/Offers';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
+      <Router>
+        <Routes>
+          <Route path="/job-search" element={<JobSearch />} />
+          <Route path="/offers" element={<Offers />} />
+          </Routes>
+      </Router>
   
-   <JobSearch />
+   
     </div>
   );
 }
