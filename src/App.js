f694e70b9cb44ca730fb0af71f89ff27bd289999
Adham ElGazoly <elgazoly11@gmail.com>
2022-09-12 18:27:32 +0200

    add chat page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 // import Register from './components/register/Register';
 // import Landing from './components/landing/Landing';
-//import Login from './components/login/Login';
+import Login from './components/login/Login';
 import JobSearch from './components/job-search/JobSearch';
 import Offers from './components/offers/Offers';
+import Chat from './components/chat/Chat';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -13,8 +14,10 @@ function App() {
     <div className="App ">
       <Router>
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/job-search" element={<JobSearch />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/chat" element={<Chat />} />
           </Routes>
       </Router>
   
